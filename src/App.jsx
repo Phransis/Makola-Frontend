429fdce78f61b077511682c95a7736b876594465
Handla <duofrancis11@gmail.com>
2025-04-30 04:24:10 +0000

    Basic ui in progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,47 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import MainLayout from './Layout/MainLayout'
+import HomePage from './pages/HomePage'
+import NotFoundPage from './pages/NotFoundPage'
 
 const App = () => {
   return (
-    <div>Makola</div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainLayout />}>
+            <Route index element={<HomePage />} />
+            <Route path='*' element={<NotFoundPage/>} />
+            {/* <Route path="/products" element={<Products />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/checkout" element={<Checkout />} /> */}
+            {/* <Route path="/products/:id" element={<ProductDetails />} /> */}
+            {/* <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} /> */}
+            {/* <Route path="/profile" element={<Profile />} /> */}
+            {/* <Route path="/orders" element={<Orders />} /> */}
+            {/* <Route path="/order/:id" element={<OrderDetails />} /> */}
+            {/* <Route path="/about" element={<About />} /> */}
+            {/* <Route path="/contact" element={<Contact />} /> */}
+            {/* <Route path="*" element={<NotFoundPage />} /> */}
+            {/* <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+            <Route path="/terms-of-service" element={<TermsOfService />} /> */}
+            {/* <Route path="/faq" element={<FAQ />} /> */}
+            {/* <Route path="/blog" element={<Blog />} />
+            <Route path="/blog/:id" element={<BlogDetails />} /> */}
+            {/* <Route path="/search" element={<SearchResults />} /> */}
+            {/* <Route path="/wishlist" element={<Wishlist />} /> */}
+            {/* <Route path="/compare" element={<Compare />} /> */}
+            {/* <Route path="/404" element={<NotFoundPage />} /> */}
+            {/* <Route path="/500" element={<ServerError />} /> */}
+            {/* <Route path="/coming-soon" element={<ComingSoon />} /> */}
+            {/* <Route path="/maintenance" element={<Maintenance />} /> */}
+            {/* <Route path="/access-denied" element={<AccessDenied />} /> */}
+            {/* <Route path="/under-construction" element={<UnderConstruction />} /> */}
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
---
+++ src/components/Footer.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+function Footer() {
+  return (
+      <div>
+            <footer className="py-3 bg-light text-center text-lg-start fixed-bottom">
+              <div className="container text-center p-3" style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
+                  <div>
+                        <a className="btn btn-outline-dark btn-floating m-1" href="#!" role="button"
+                            ><i className="fab fa-facebook-f"></i
+                        ></a>
+    
+                        <a className="btn btn-outline-dark btn-floating m-1" href="#!" role="button"
+                            ><i className="fab fa-twitter"></i
+                        ></a>
+    
+                        <a className="btn btn-outline-dark btn-floating m-1" href="#!" role="button"
+                            ><i className="fab fa-google"></i
+                        ></a>
+    
+                        <a className="btn btn-outline-dark btn-floating m-1" href="#!" role="button"
+                            ><i className="fab fa-instagram"></i
+                        ></a>
+    
+                        <a className="btn btn-outline-dark btn-floating m-1" href="#!" role="button"
+                            ><i className="fab fa-linkedin"></i
+                        ></a>
+    
+                        <a className="btn btn-outline-dark btn-floating m-1" href="#!" role="button"
+                            ><i className="fab fa-github"></i
+                        ></a>
+                  </div>
+                    © 2023 Makola:
+                    <a className="text-dark" href="https://mdbootstrap.com/"> MDBootstrap.com</a>
+                </div>
+            </footer>
+    </div>
+  )
+}
+
+export default Footer
